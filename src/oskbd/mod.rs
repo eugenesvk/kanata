9cd414ed5b188db3b9f9aa0246baed20dc7e063e
eugenesvk <eugenesvk@users.noreply.github.com>
2024-03-15 16:29:28 +0700

    condense kbd KeyValue

--- src/oskbd/mod.rs
+++ src/oskbd/mod.rs
@@ -1,69 +1,26 @@
 //! Platform specific code for low level keyboard read/write.
-
-#[cfg(target_os="linux")]
-mod linux;
-#[cfg(target_os="linux")]
-pub use linux::*;
-
-#[cfg(target_os="windows")]
-mod windows;
-#[cfg(target_os="windows")]
-pub use windows::*;
-
-#[cfg(target_os="macos")]
-mod macos;
-#[cfg(target_os="macos")]
-pub use macos::*;
-
+#[cfg(target_os="linux"  	)] mod linux;
+#[cfg(target_os="windows"	)] mod windows;
+#[cfg(target_os="macos"  	)] mod macos;
+#[cfg(target_os="linux"  	)] pub use linux::*;
+#[cfg(target_os="windows"	)] pub use windows::*;
+#[cfg(target_os="macos"  	)] pub use macos::*;
 // ------------------ KeyValue --------------------
-
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
-pub enum KeyValue {
-    Release = 0,
-    Press = 1,
-    Repeat = 2,
-    Tap,
-    WakeUp,
-}
-
-impl From<i32> for KeyValue {
-    fn from(item: i32) -> Self {
-        match item {
-            0 => Self::Release,
-            1 => Self::Press,
-            2 => Self::Repeat,
-            _ => unreachable!(),
-        }
-    }
-}
-
-impl From<bool> for KeyValue {
-    fn from(up: bool) -> Self {
-        match up {
-            true => Self::Release,
-            false => Self::Press,
-        }
-    }
-}
-
-impl From<KeyValue> for bool {
-    fn from(val: KeyValue) -> Self {
-        matches!(val, KeyValue::Release)
-    }
-}
+#[derive(Copy,Clone,Debug,PartialEq,Eq)] pub enum KeyValue {Release=0,Press=1,Repeat=2,Tap,WakeUp,}
+impl From<i32     > for KeyValue {fn from(item:i32     ) -> Self { match item {
+  0 => Self::Release,
+  1 => Self::Press  ,
+  2 => Self::Repeat ,
+  _ => unreachable!(),}}}
+impl From<bool    > for KeyValue {fn from(up  :bool    ) -> Self { match up {
+  true  => Self::Release,
+  false => Self::Press  ,}}}
+impl From<KeyValue> for bool     {fn from(val :KeyValue) -> Self { matches!(
+  val, KeyValue::Release)}}
 
 use kanata_parser::keys::OsCode;
-
-#[derive(Debug, Clone, Copy)]
-pub struct KeyEvent {
-    pub code: OsCode,
-    pub value: KeyValue,
-}
-
-#[cfg(not(all(feature="interception_driver", target_os="windows")))]
-#[cfg(not(target_os="macos"))]
-impl KeyEvent {
-    pub fn new(code: OsCode, value: KeyValue) -> Self {
-        Self { code, value }
-    }
-}
+#[derive(Debug,Clone,Copy)] pub struct KeyEvent {
+  pub code : OsCode,
+  pub value: KeyValue,}
+#[cfg(not(all(feature="interception_driver",target_os="windows")))] #[cfg(not(target_os="macos"))]
+impl KeyEvent { pub fn new(code:OsCode, value:KeyValue) -> Self { Self {code,value} }}
