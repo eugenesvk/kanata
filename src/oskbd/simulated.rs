d13f857cf0ec0a1cdf7dd2f78467c55fff667f3d
eugenesvk <eugenesvk@users.noreply.github.com>
2024-03-19 03:13:47 +0700

    condense

--- src/oskbd/simulated.rs
+++ src/oskbd/simulated.rs
@@ -27,22 +27,23 @@ impl KbdOut {
         let key_ev = KeyEvent::new(key, value);
         let event = {#[cfg(    target_os = "macos" )]{key_ev.try_into().unwrap()}
                      #[cfg(not(target_os = "macos"))]{key_ev.into()             }  };
-        self.write(event)}
+        self.write(event)
+      }
     pub fn write_code  (&mut self, code: u32, value: KeyValue) -> Result<(), io::Error> {println!("out-code:{code};{value:?}");Ok(())}
-    pub fn press_key   (&mut self, key: OsCode) -> Result<(),io::Error> {self.write_key(key,KeyValue::Press  )}
-    pub fn release_key (&mut self, key: OsCode) -> Result<(),io::Error> {self.write_key(key,KeyValue::Release)}
-    pub fn send_unicode(&mut self, c  : char  ) -> Result<(),io::Error> {println!("outU:{c}") ;Ok(())}
+    pub fn press_key   (&mut self, key: OsCode) -> Result<(),io::Error> {self.log.fmt(LogFmtT::KeyDown,key.to_string());self.write_key(key,KeyValue::Press  )}
+    pub fn release_key (&mut self, key: OsCode) -> Result<(),io::Error> {self.log.fmt(LogFmtT::KeyUp  ,key.to_string());self.write_key(key,KeyValue::Release)}
+    pub fn send_unicode(&mut self, c  : char  ) -> Result<(),io::Error> {println!("outU:{c}"); self.log.fmt(LogFmtT::Unicode,c.to_string());Ok(())}
     pub fn click_btn   (&mut self, btn: Btn   ) -> Result<(),io::Error> {println!("out🖰:↓{btn:?}");Ok(())}
     pub fn release_btn (&mut self, btn: Btn   ) -> Result<(),io::Error> {println!("out🖰:↑{btn:?}");Ok(())}
     pub fn scroll(&mut self, direction: MWheelDirection, distance: u16) -> Result<(), io::Error> {
         println!("scroll:{direction:?},{distance:?}");Ok(())}
     pub fn move_mouse(&mut self, mv: CalculatedMouseMove) -> Result<(), io::Error> {
                          let (direction, distance) = ( mv.direction,  mv.distance);
-            println!("mouse-move:{direction:?},{distance:?}");Ok(())}
+            println!("out🖰:move {direction:?},{distance:?}");Ok(())}
     pub fn move_mouse_many(&mut self, moves: &[CalculatedMouseMove]) -> Result<(), io::Error> {
         for mv in moves {let (direction, distance) = (&mv.direction, &mv.distance);
-            println!("mouse-move:{direction:?},{distance:?}");}Ok(())}
-    pub fn set_mouse(&mut self, x: u16, y: u16) -> Result<(), io::Error> {log::info!("set-mouse:{x},{y}");Ok(())}
+            println!("out🖰:move {direction:?},{distance:?}");}Ok(())}
+    pub fn set_mouse(&mut self, x: u16, y: u16) -> Result<(), io::Error> {log::info!("out🖰:@{x},{y}");Ok(())}
 }
 
 #[cfg(not(any(target_os="windows",target_os="macos")))]
