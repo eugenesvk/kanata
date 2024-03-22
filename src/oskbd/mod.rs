7fd5db0558bb5654b131af7458f6346d65f82a1e
eugenesvk <eugenesvk@users.noreply.github.com>
2024-03-23 01:56:28 +0700

    format

--- src/oskbd/mod.rs
+++ src/oskbd/mod.rs
@@ -1,11 +1,11 @@
 #![allow(non_snake_case)]
 //! Platform specific code for low level keyboard read/write.
-#[cfg(target_os="linux"         	)] mod linux;
-#[cfg(target_os="windows"       	)] mod windows;
-#[cfg(target_os="macos"         	)] mod macos;
-#[cfg(target_os="linux"         	)] pub use linux::*;
-#[cfg(target_os="windows"       	)] pub use windows::*;
-#[cfg(target_os="macos"         	)] pub use macos::*;
+#[cfg(target_os="linux"  	)] mod linux;
+#[cfg(target_os="windows"	)] mod windows;
+#[cfg(target_os="macos"  	)] mod macos;
+#[cfg(target_os="linux"  	)] pub use linux::*;
+#[cfg(target_os="windows"	)] pub use windows::*;
+#[cfg(target_os="macos"  	)] pub use macos::*;
 #[cfg(feature="simulated_output"	)] mod simulated;
 #[cfg(feature="simulated_output"	)] pub use simulated::*;
 
