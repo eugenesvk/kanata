9f4bdab36d00095e0dcd18b512827d10831f9bed
eugenesvk <eugenesvk@users.noreply.github.com>
2024-04-19 01:15:16 +0700

    feat(gui): fix main

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use crate::m_cli::main_cli;
 
 use anyhow::{Result};
 #[cfg(not(feature = "gui"))]
+use anyhow::{Result};
+#[cfg(not(feature = "gui"))]
 fn main() -> Result<()> {
     let ret = main_cli();
     ret
@@ -18,5 +20,5 @@ fn main() -> Result<()> {
 
 #[cfg(feature = "gui")]
 fn main() {
-    main_gui()
+    main_gui();
 }
