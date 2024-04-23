4e3c0e13df90156cc0213de488b7f50827ce44db
eugenesvk <eugenesvk@users.noreply.github.com>
2024-04-23 23:41:50 +0700

    ___gui: temp experiment

--- src/kanata/windows/llhook.rs
+++ src/kanata/windows/llhook.rs
@@ -1,4 +1,5 @@
 #![allow(non_snake_case)]
+use core::cell::RefCell;
 use anyhow::Context;
 use parking_lot::Mutex;
 use std::convert::TryFrom;
@@ -71,8 +72,44 @@ impl Kanata {
             true
         });
         #[cfg(feature = "gui")]
-        let _ui = SystemTray::build_ui(Default::default()).context("Failed to build UI")?;
+        // let _ui = SystemTray::build_ui(Default::default()).context("Failed to build UI")?;
+        let app_data = SystemTrayData {ttt:"✗✗✗".to_string()};
+        let app : SystemTray = SystemTray {data:RefCell::new(app_data), ..Default::default()};
+
+        // let mut app : SystemTray = SystemTray {data:RefCell::new(app_data), ..Default::default()};
+        // let _a = &app.tray_item1;
+        // info!("{:?}",_a.handle);
+        // info!("tray_item1 pre add {:?}",_a.handle);
+        // let _a = &app.tray_item4;
+        // let _ = app.add_menu();
+        // let _a = &app.tray_item1;
+        // info!("tray_item1 pos add {:?}",_a.handle);
+        let mut _ui = SystemTray::build_ui(app).context("Failed to build UI")?;
+        // let _ = _ui.inner.borrow_mut().add_menu();
+        // let _ = _ui.tray_item1.drop();
+
+        // nwg::Font::set_global_family("Segoe UI").context("Failed to set default font")?;
+        // let _ui = MessageBank::build_ui(Default::default()).context("Failed to build UI")?;
+        // _ui.add_message();
+        // _ui.add_message();
+        // _ui.add_message();
+        // _ui.add_message();
+
+
+        // let ui_inner = _ui.inner.borrow();
+        // match _ui.hmenu_item() {
+            // Some(mi1) => {mi1.text = "saD".into();},
+            // None => {},
+        // };
+            // nwg::MenuItem::builder()
+                // .text("Hello")
+                // .parent(&_ui.trzay_menu)
+                // .build(&mut _ui.tray_item1)?;
+                // .build(&mut ui.data.tray_item1)?;
+
+
         native_windows_gui::dispatch_thread_events(); // The event loop is also required for the low-level keyboard hook to work.
+        // app.data.borrow_mut().close(); // close the shared memory handle once we are done
         //TEMP todo disable// eprintln!("\nPress enter to exit"); // moved from main to not panic on a disconnected channel
         //TEMP todo disable// let _ = std::io::stdin().read_line(&mut String::new());
         Ok(())
