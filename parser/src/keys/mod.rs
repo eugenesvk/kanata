0b6b958436aa87909e95821e5eebb8eb88216db0
eugenesvk <eugenesvk@users.noreply.github.com>
2024-04-28 02:55:03 +0700

    feat: allow `<` as alias for `lsgt` (#965)
    Allows `<` as alias for `lsgt` (non-US backslash). There is an existing
    `﹨` alias but it's non-ASCII, doesn't represent the actual symbol (on my
    keyboard, at least) and the character isn't present even in some "big"
    fonts.

--- parser/src/keys/mod.rs
+++ parser/src/keys/mod.rs
@@ -162,7 +162,7 @@ pub fn str_to_oscode(s: &str) -> Option<OsCode> {
     "Escape"|"esc"|"⎋"                                         	=> OsCode::KEY_ESC,
     "Space"|"spc"|"␠"|"␣"                                      	=> OsCode::KEY_SPACE,
     "ssrq"|"sys"                                               	=> OsCode::KEY_SYSRQ,
-    "IntlBackslash"|"102d"|"lsgt"|"nubs"|"nonusbslash"|"﹨"     	=> OsCode::KEY_102ND, // Typically the Non-US backslash, near the left shift key
+    "IntlBackslash"|"102d"|"lsgt"|"nubs"|"nonusbslash"|"﹨"|"<" 	=> OsCode::KEY_102ND, // Typically the Non-US backslash, near the left shift key
     "ScrollLock"|"scrlck"|"slck"|"⇳🔒"                          	=> OsCode::KEY_SCROLLLOCK,
     "Pause"|"pause"|"break"|"brk"                              	=> OsCode::KEY_PAUSE,
     "WakeUp"|"wkup"                                            	=> OsCode::KEY_WAKEUP,
