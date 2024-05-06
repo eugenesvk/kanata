7327d73530531535721f6cbb67365ea9accecb47
eugenesvk <eugenesvk@users.noreply.github.com>
2024-05-06 21:43:20 +0700

    reduce cfg usage in main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,179 +6,181 @@
 
 
 #![allow(non_upper_case_globals)]
-use anyhow::{bail, Result};
-use clap::{Parser};
-use kanata_parser::cfg;
 use crate::*;
-use log::info;
-use simplelog::{format_description, *};
-
-use std::path::PathBuf;
-
 #[cfg(test)]
 mod tests;
 
-#[derive(Parser, Debug)]
-#[command(author, version, verbatim_doc_comment)]
-/// kanata: an advanced software key remapper
-///
-/// kanata remaps key presses to other keys or complex actions depending on the
-/// configuration for that key. You can find the guide for creating a config
-/// file here:
-///
-///     https://github.com/jtroo/kanata/blob/main/docs/config.adoc
-///
-/// If you need help, please feel welcome to create an issue or discussion in
-/// the kanata repository:
-///
-///     https://github.com/jtroo/kanata
-struct Args {
-  // Display different platform specific paths based on the target OS
-  #[cfg_attr(
-    target_os = "windows",
-    doc = r"Configuration file(s) to use with kanata. If not specified, defaults to
-kanata.kbd in the current working directory and
-'C:\Users\user\AppData\Roaming\kanata\kanata.kbd'"
-  )]
-  #[cfg_attr(
-    target_os = "macos",
-    doc = "Configuration file(s) to use with kanata. If not specified, defaults to
-kanata.kbd in the current working directory and
-'$HOME/Library/Application Support/kanata/kanata.kbd.'"
-  )]
-  #[cfg_attr(
-    not(any(target_os = "macos", target_os = "windows")),
-    doc = "Configuration file(s) to use with kanata. If not specified, defaults to
-kanata.kbd in the current working directory and
-'$XDG_CONFIG_HOME/kanata/kanata.kbd'"
-  )]
-  #[arg(short, long, verbatim_doc_comment)]
-  cfg: Option<Vec<PathBuf>>,
+#[cfg(not(feature = "gui"))]
+mod cli {
+    pub(crate) use anyhow::{bail, Result};
+    pub(crate) use clap::Parser;
+    pub(crate) use kanata_parser::cfg;
+    pub(crate) use kanata_state_machine::*;
+    pub(crate) use log::info;
+    pub(crate) use simplelog::{format_description, *};
+    pub(crate) use std::path::PathBuf;
+    #[derive(Parser, Debug)]
+    #[command(author, version, verbatim_doc_comment)]
+    /// kanata: an advanced software key remapper
+    ///
+    /// kanata remaps key presses to other keys or complex actions depending on the
+    /// configuration for that key. You can find the guide for creating a config
+    /// file here:
+    ///
+    ///     https://github.com/jtroo/kanata/blob/main/docs/config.adoc
+    ///
+    /// If you need help, please feel welcome to create an issue or discussion in
+    /// the kanata repository:
+    ///
+    ///     https://github.com/jtroo/kanata
+    struct Args {
+      // Display different platform specific paths based on the target OS
+      #[cfg_attr(
+        target_os = "windows",
+        doc = r"Configuration file(s) to use with kanata. If not specified, defaults to
+    kanata.kbd in the current working directory and
+    'C:\Users\user\AppData\Roaming\kanata\kanata.kbd'"
+      )]
+      #[cfg_attr(
+        target_os = "macos",
+        doc = "Configuration file(s) to use with kanata. If not specified, defaults to
+    kanata.kbd in the current working directory and
+    '$HOME/Library/Application Support/kanata/kanata.kbd.'"
+      )]
+      #[cfg_attr(
+        not(any(target_os = "macos", target_os = "windows")),
+        doc = "Configuration file(s) to use with kanata. If not specified, defaults to
+    kanata.kbd in the current working directory and
+    '$XDG_CONFIG_HOME/kanata/kanata.kbd'"
+      )]
+      #[arg(short, long, verbatim_doc_comment)]
+      cfg: Option<Vec<PathBuf>>,
 
-  /// Port to run the optional TCP server on. If blank, no TCP port will be
-  /// listened on.
-  #[cfg(feature = "tcp_server")]
-  #[arg(
-    short = 'p',
-    long = "port",
-    value_name = "PORT or IP:PORT",
-    verbatim_doc_comment
-  )]
-  tcp_server_address: Option<SocketAddrWrapper>,
+      /// Port to run the optional TCP server on. If blank, no TCP port will be
+      /// listened on.
+      #[cfg(feature = "tcp_server")]
+      #[arg(
+        short = 'p',
+        long = "port",
+        value_name = "PORT or IP:PORT",
+        verbatim_doc_comment
+      )]
+      tcp_server_address: Option<SocketAddrWrapper>,
 
-  /// Path for the symlink pointing to the newly-created device. If blank, no
-  /// symlink will be created.
-  #[cfg(target_os = "linux")]
-  #[arg(short, long, verbatim_doc_comment)]
-  symlink_path: Option<String>,
+      /// Path for the symlink pointing to the newly-created device. If blank, no
+      /// symlink will be created.
+      #[cfg(target_os = "linux")]
+      #[arg(short, long, verbatim_doc_comment)]
+      symlink_path: Option<String>,
 
-  /// List the keyboards available for grabbing and exit.
-  #[cfg(target_os = "macos")]
-  #[arg(short, long)]
-  list: bool,
+      /// List the keyboards available for grabbing and exit.
+      #[cfg(target_os = "macos")]
+      #[arg(short, long)]
+      list: bool,
 
-  /// Enable debug logging.
-  #[arg(short, long)]
-  debug: bool,
+      /// Enable debug logging.
+      #[arg(short, long)]
+      debug: bool,
 
-  /// Enable trace logging; implies --debug as well.
-  #[arg(short, long)]
-  trace: bool,
+      /// Enable trace logging; implies --debug as well.
+      #[arg(short, long)]
+      trace: bool,
 
-  /// Remove the startup delay on kanata.
-  /// In some cases, removing the delay may cause keyboard issues on startup.
-  #[arg(short, long, verbatim_doc_comment)]
-  nodelay: bool,
+      /// Remove the startup delay on kanata.
+      /// In some cases, removing the delay may cause keyboard issues on startup.
+      #[arg(short, long, verbatim_doc_comment)]
+      nodelay: bool,
 
-  /// Milliseconds to wait before attempting to register a newly connected
-  /// device. The default is 200.
-  ///
-  /// You may wish to increase this if you have a device that is failing
-  /// to register - the device may be taking too long to become ready.
-  #[cfg(target_os = "linux")]
-  #[arg(short, long, verbatim_doc_comment)]
-  wait_device_ms: Option<u64>,
+      /// Milliseconds to wait before attempting to register a newly connected
+      /// device. The default is 200.
+      ///
+      /// You may wish to increase this if you have a device that is failing
+      /// to register - the device may be taking too long to become ready.
+      #[cfg(target_os = "linux")]
+      #[arg(short, long, verbatim_doc_comment)]
+      wait_device_ms: Option<u64>,
 
-  /// Validate configuration file and exit
-  #[arg(long, verbatim_doc_comment)]
-  check: bool,
-}
-
-/// Parse CLI arguments and initialize logging.
-fn cli_init() -> Result<ValidatedArgs> {
-  let args = Args::parse();
-
-  #[cfg(target_os = "macos")]
-  if args.list {
-    karabiner_driverkit::list_keyboards();
-    std::process::exit(0);
-  }
-
-  let cfg_paths = args.cfg.unwrap_or_else(default_cfg);
-
-  let log_lvl = match (args.debug, args.trace) {
-    (_, true) => LevelFilter::Trace,
-    (true, false) => LevelFilter::Debug,
-    (false, false) => LevelFilter::Info,
-  };
-
-  let mut log_cfg = ConfigBuilder::new();
-  if let Err(e) = log_cfg.set_time_offset_to_local() {
-    eprintln!("WARNING: could not set log TZ to local: {e:?}");
-  };
-  log_cfg.set_time_format_custom(format_description!(version=2,"[minute]:[second].[subsecond digits:3]"));
-  #[cfg(all(not(target_os = "windows"), not(feature = "gui")))]
-    CombinedLogger::init(vec![TermLogger::new(log_lvl,log_cfg.build(),TerminalMode::Mixed,ColorChoice::AlwaysAnsi,
-    )]).expect("logger can init");
-  #[cfg(all(    target_os = "windows",      feature = "gui" ))]
-  if *IS_TERM	{
-    CombinedLogger::init(vec![TermLogger::new(log_lvl,log_cfg.build(),TerminalMode::Mixed,ColorChoice::AlwaysAnsi,),
-      log_win::windbg_simple_combo(log_lvl),]).expect("logger can init");
-  } else {CombinedLogger::init(vec![log_win::windbg_simple_combo(log_lvl),]).expect("logger can init");}
-  log::info!("kanata v{} starting", env!("CARGO_PKG_VERSION"));
-  #[cfg(all(not(feature = "interception_driver"), target_os = "windows"))]
-  log::info!("using LLHOOK+SendInput for keyboard IO");
-  #[cfg(all(feature = "interception_driver", target_os = "windows"))]
-  log::info!("using the Interception driver for keyboard IO");
-
-  if let Some(config_file) = cfg_paths.first() {
-    if !config_file.exists() {
-      bail!(
-    "Could not find the config file ({})\nFor more info, pass the `-h` or `--help` flags.",
-    cfg_paths[0].to_str().unwrap_or("?")
-   )
+      /// Validate configuration file and exit
+      #[arg(long, verbatim_doc_comment)]
+      check: bool,
     }
-  } else {
-    bail!("No config files provided\nFor more info, pass the `-h` or `--help` flags.");
-  }
 
-  if args.check {
-    log::info!("validating config only and exiting");
-    let status = match cfg::new_from_file(&cfg_paths[0]) {
-      Ok(_) => 0,
-      Err(e) => {
-        log::error!("{e:?}");
-        1
-      }
+  /// Parse CLI arguments and initialize logging.
+  fn cli_init() -> Result<ValidatedArgs> {
+    let args = Args::parse();
+
+    #[cfg(target_os = "macos")]
+    if args.list {
+      karabiner_driverkit::list_keyboards();
+      std::process::exit(0);
+    }
+
+    let cfg_paths = args.cfg.unwrap_or_else(default_cfg);
+
+    let log_lvl = match (args.debug, args.trace) {
+      (_, true) => LevelFilter::Trace,
+      (true, false) => LevelFilter::Debug,
+      (false, false) => LevelFilter::Info,
     };
-    std::process::exit(status);
-  }
 
-  #[cfg(target_os = "linux")]
-  if let Some(wait) = args.wait_device_ms {
-    use std::sync::atomic::Ordering;
-    log::info!("Setting device registration wait time to {wait} ms.");
-    oskbd::WAIT_DEVICE_MS.store(wait, Ordering::SeqCst);
-  }
+    let mut log_cfg = ConfigBuilder::new();
+    if let Err(e) = log_cfg.set_time_offset_to_local() {
+      eprintln!("WARNING: could not set log TZ to local: {e:?}");
+    };
+    log_cfg.set_time_format_custom(format_description!(version=2,"[minute]:[second].[subsecond digits:3]"));
+    #[cfg(all(not(target_os = "windows"), not(feature = "gui")))]
+      CombinedLogger::init(vec![TermLogger::new(log_lvl,log_cfg.build(),TerminalMode::Mixed,ColorChoice::AlwaysAnsi,
+      )]).expect("logger can init");
+    #[cfg(all(    target_os = "windows",      feature = "gui" ))]
+    if *IS_TERM	{
+      CombinedLogger::init(vec![TermLogger::new(log_lvl,log_cfg.build(),TerminalMode::Mixed,ColorChoice::AlwaysAnsi,),
+        log_win::windbg_simple_combo(log_lvl),]).expect("logger can init");
+    } else {CombinedLogger::init(vec![log_win::windbg_simple_combo(log_lvl),]).expect("logger can init");}
+    log::info!("kanata v{} starting", env!("CARGO_PKG_VERSION"));
+    #[cfg(all(not(feature = "interception_driver"), target_os = "windows"))]
+    log::info!("using LLHOOK+SendInput for keyboard IO");
+    #[cfg(all(feature = "interception_driver", target_os = "windows"))]
+    log::info!("using the Interception driver for keyboard IO");
 
-  Ok(ValidatedArgs {
-    paths: cfg_paths,
-    #[cfg(feature = "tcp_server")]
-    tcp_server_address: args.tcp_server_address,
+    if let Some(config_file) = cfg_paths.first() {
+      if !config_file.exists() {
+        bail!(
+      "Could not find the config file ({})\nFor more info, pass the `-h` or `--help` flags.",
+      cfg_paths[0].to_str().unwrap_or("?")
+     )
+      }
+    } else {
+      bail!("No config files provided\nFor more info, pass the `-h` or `--help` flags.");
+    }
+
+    if args.check {
+      log::info!("validating config only and exiting");
+      let status = match cfg::new_from_file(&cfg_paths[0]) {
+        Ok(_) => 0,
+        Err(e) => {
+          log::error!("{e:?}");
+          1
+        }
+      };
+      std::process::exit(status);
+    }
+
     #[cfg(target_os = "linux")]
-    symlink_path: args.symlink_path,
-    nodelay: args.nodelay,
-  })
+    if let Some(wait) = args.wait_device_ms {
+      use std::sync::atomic::Ordering;
+      log::info!("Setting device registration wait time to {wait} ms.");
+      oskbd::WAIT_DEVICE_MS.store(wait, Ordering::SeqCst);
+    }
+
+    Ok(ValidatedArgs {
+      paths: cfg_paths,
+      #[cfg(feature = "tcp_server")]
+      tcp_server_address: args.tcp_server_address,
+      #[cfg(target_os = "linux")]
+      symlink_path: args.symlink_path,
+      nodelay: args.nodelay,
+    })
+  }
 }
 
 fn main_impl() -> Result<()> {
@@ -226,8 +228,7 @@ fn main_impl() -> Result<()> {
 
 
 #[cfg(not(feature = "gui"))]
-use anyhow::{Result};
-#[cfg(not(feature = "gui"))]
+use cli::*;
 pub fn main() -> Result<()> {
     let ret = main_impl();
     if let Err(ref e) = ret {
@@ -241,5 +242,6 @@ pub fn main() -> Result<()> {
 
 #[cfg(feature = "gui")]
 fn main() {
+    use kanata_state_machine::gui::lib_main_gui;
     lib_main_gui();
 }
