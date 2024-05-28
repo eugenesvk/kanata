087df9a057f8a4ac1f939ec1375a19b7d6bba45e
eugenesvk <eugenesvk@users.noreply.github.com>
2024-05-29 01:29:01 +0700

    feat: add a custom action to send a single number via TCP

--- src/kanata/mod.rs
+++ src/kanata/mod.rs
@@ -1282,7 +1282,36 @@ impl Kanata {
                             #[cfg(not(feature = "tcp_server"))]
                             log::warn!(
                                 "{} or {} was used, but Kanata was compiled with TCP server disabled.",
-                                PUSH_MESSAGE_S,PUSH_MESSAGE_S_A
+                                PUSH_MESSAGE_N,PUSH_MESSAGE_N_A,
+                            );
+                        }
+                        CustomAction::PushMessageN(_argu) => {
+                            log::debug!("Action push-msg-nn");
+                            #[cfg(feature = "tcp_server")]
+                            if let Some(tx) = _tx {
+                                log::debug!("Action push-msg message: {}", _argu);
+                                match tx.try_send(ServerMessage::MessagePushN(*_argu)) {
+                                    Ok(_) => {}
+                                    Err(error) => {
+                                        log::error!(
+                                            "could not send {} or {} event notification: {}",
+                                            PUSH_MESSAGE_N,PUSH_MESSAGE_N_A,
+                                            error
+                                        );
+                                    }
+                                }
+                            }
+                            #[cfg(feature = "tcp_server")]
+                            match self.tcp_server_address {
+                                None => {
+                                    log::warn!("{} or {} was used, but TCP server is not running. did you specify a port?", PUSH_MESSAGE_N,PUSH_MESSAGE_N_A);
+                                }
+                                Some(_) => {}
+                            }
+                            #[cfg(not(feature = "tcp_server"))]
+                            log::warn!(
+                                "{} or {} was used, but Kanata was compiled with TCP server disabled.",
+                                PUSH_MESSAGE_N,PUSH_MESSAGE_N_A
                             );
                         }
                         CustomAction::WinSendMessage(_win_msg) => {
