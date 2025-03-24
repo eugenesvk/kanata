d749835dd3767e436fc06f46b43cfa2648dd304c
eugenesvk <eugenesvk@users.noreply.github.com>
2025-03-24 20:53:51 +0700

    misc(macos): suppress warnings

--- src/oskbd/macos.rs
+++ src/oskbd/macos.rs
@@ -1,4 +1,6 @@
 //! Contains the input/output code for keyboards on Macos.
+// Caused by unmaintained objc crate triggering warnings.
+#![allow(unexpected_cfgs)]
 #![cfg_attr(
     feature = "simulated_output",
     allow(dead_code, unused_imports, unused_variables, unused_mut)
