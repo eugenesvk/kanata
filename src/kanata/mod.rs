da9b66bbe3131f0a69f935dc1b442f6469ef8546
jtroo <j.andreitabs@gmail.com>
2025-04-20 11:21:34 -0700

    misc: clippy

--- src/kanata/mod.rs
+++ src/kanata/mod.rs
@@ -1541,7 +1541,7 @@ impl Kanata {
                         | CustomAction::Unmodded { .. }
                         | CustomAction::Unshifted { .. }
                         // Note: ReverseReleaseOrder is already handled earlier on.
-                        | CustomAction::ReverseReleaseOrder { .. }
+                        | CustomAction::ReverseReleaseOrder
                         | CustomAction::CancelMacroOnRelease => {}
                     }
                 }
